ed4de4626e4cab1ec3856e6ba5cc1366e387e7c7
__K__ <65128789+k722003@users.noreply.github.com>
2021-06-22 13:44:17 +0530

    Optimization to use less memory
    Changed the converted list variable used to temporarily store converted images into a gen expr which uses less memory

--- src/PDFHandler.py
+++ src/PDFHandler.py
@@ -1,11 +1,6 @@
-from PIL import Image
-
 class PDFHandler:
     @staticmethod
-    def save_to_pdf(images: list, output_path):
-        converted = []
-        for img in images:
-            converted.append(img.convert('RGBA').convert('RGB'))
-        first_page = converted[0]
-        converted.remove(first_page)
+    def save_to_pdf(images, output_path):
+        converted = (img.convert('RGBA').convert('RGB') for img in images)
+        first_page = next(converted)
         first_page.save(output_path, save_all=True, append_images=converted)
