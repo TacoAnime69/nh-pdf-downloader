c58fc03e4339a8b584cb432ac1bbf7cec6d023ac
__K__ <65128789+KThankYou@users.noreply.github.com>
2022-05-13 17:27:33 +0530

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,9 +1,9 @@
 from PIL import Image
-from sys import path, platform
-from zipfile import ZipFile
-from collections import defaultdict
-from multiprocessing import Queue
 from time import sleep
+from sys import platform
+from zipfile import ZipFile
+from multiprocessing import Queue
+from collections import defaultdict
 import os, shutil, datetime, threading, re
 
 from .DownloadHandler import DownloadHandler
